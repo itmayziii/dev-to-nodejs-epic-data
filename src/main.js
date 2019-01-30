7f321418731c991544e4918b5e93e44b97e38623
itmayziii <tommymay37@gmail.com>
2019-01-29 22:44:06 -0500

    Added pipeline

--- src/main.js
+++ src/main.js
@@ -2,15 +2,24 @@ const fs = require('fs')
 const path = require('path')
 const CsvParser = require('csv-parse')
 const TransformUsernameToGithubRepos = require('./transform-username-to-github-repos')
+const stream = require('stream')
 
 const readGithubUsernamesStream = fs.createReadStream(path.resolve(__dirname, '../github-usernames.csv'))
 const csvParser = new CsvParser({ columns: false })
 const transformUsernameToGithubRepos = new TransformUsernameToGithubRepos()
 const writeStream = fs.createWriteStream(path.resolve(__dirname, '../github-user-repositories.txt'))
 
-let githubUserRepositories = []
-readGithubUsernamesStream
-  .pipe(csvParser)
-  .pipe(transformUsernameToGithubRepos)
-  .pipe(writeStream)
-  .on('end', () => process.exit())
+stream.pipeline(
+  readGithubUsernamesStream,
+  csvParser,
+  transformUsernameToGithubRepos,
+  writeStream,
+  (error) => {
+    if (error) {
+      console.error('error ', error)
+      return process.exit(1)
+    }
+
+    process.exit()
+  }
+)
