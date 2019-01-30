5fd14b4e49e39e22d2bc5dbf54a6f16bc56e59f7
itmayziii <tommymay37@gmail.com>
2019-01-29 22:32:56 -0500

    3rd and 4th step

--- src/main.js
+++ src/main.js
@@ -1,26 +1,16 @@
 const fs = require('fs')
 const path = require('path')
+const CsvParser = require('csv-parse')
+const TransformUsernameToGithubRepos = require('./transform-username-to-github-repos')
 
-const readGithubUsernamesStream = fs.createReadStream(path.resolve(__dirname, '../github-usernames.json'))
+const readGithubUsernamesStream = fs.createReadStream(path.resolve(__dirname, '../github-usernames.csv'))
+const csvParser = new CsvParser({ columns: false })
+const transformUsernameToGithubRepos = new TransformUsernameToGithubRepos()
+const writeStream = fs.createWriteStream(path.resolve(__dirname, '../github-user-repositories.txt'))
 
-let githubUsernames = ''
-readGithubUsernamesStream.on('data', (data) => githubUsernames += data)
-readGithubUsernamesStream.on('end', () => console.log(JSON.parse(githubUsernames)))
-// Outputs
-/*
-  ['itmayziii',
-  'dhershman1',
-  'HetaRZinzuvadia',
-  'joeswislocki',
-  'justinvoelkel',
-  'mandarm2593',
-  'mfrost503',
-  'wmontgomery',
-  'kentcdodds',
-  'gaearon',
-  'btholt',
-  'paulirish',
-  'ryanflorence']
- */
-
-
+let githubUserRepositories = []
+readGithubUsernamesStream
+  .pipe(csvParser)
+  .pipe(transformUsernameToGithubRepos)
+  .pipe(writeStream)
+  .on('end', () => process.exit())
--- src/transform-username-to-github-repos.js
+++ src/transform-username-to-github-repos.js
@@ -1,1 +1,60 @@
-class
+const axios = require('axios')
+const stream = require('stream')
+
+module.exports = class TransformUsernameToGithubRepos extends stream.Transform {
+  constructor (options = {}) {
+    super({ ...options, objectMode: true })
+    this.requests = []
+  }
+
+  _transform (chunk, encoding, callback) {
+    const username = chunk[0]
+    const githubRequest = this.getGithubRepositoriesForUser(username)
+    this.requests.push(this.prepareGithubRequest(username, githubRequest))
+    if (this.requests.length < 5) {
+      return callback()
+    }
+
+    this.processRequests(callback)
+  }
+
+  _flush (callback) {
+    this.processRequests(callback)
+  }
+
+  getGithubRepositoriesForUser (username) {
+    return axios.get(`https://api.github.com/users/${username}/repos`, {
+      headers: {
+        Authorization: `Token ${process.env.GITHUB_ACCESS_TOKEN}`
+      }
+    })
+  }
+
+  prepareGithubRequest (username, githubRequest) {
+    return githubRequest
+      .then((response) => {
+        let repositories = []
+        if (response.data) {
+          repositories = response.data.map((repository) => repository.name)
+        }
+
+        return {
+          username,
+          repositories
+        }
+      })
+  }
+
+  processRequests (callback) {
+    return Promise.all(this.requests)
+      .then((responses) => {
+        this.requests = []
+
+        this.push(responses.reduce((accumulator, currentValue) => {
+          return accumulator + JSON.stringify(currentValue)
+        }, ''))
+        callback()
+      })
+      .catch(callback)
+  }
+}
